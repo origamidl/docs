48fd427767d88d899afa90688df14bb47bb5b000
Christopher Mühl <christopher@padarom.io>
2018-06-01 13:40:24 +0200

    Create cross-platform framework and macOS CLI

---
+++ Sources/CLI/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  CLI
+//
+//  Created by Christopher Mühl on 01.06.18.
+//
+
+import Foundation
+import Origami
+
+let origami = Origami()
+
+print(origami)
---
+++ Sources/Origami/Origami.swift
@@ -0,0 +1,17 @@
+//
+//  Origami.swift
+//  Origami
+//
+//  Created by Christopher Mühl on 01.06.18.
+//
+
+
+import Foundation
+
+public class Origami {
+
+    public init() {
+        
+    }
+
+}
